fbeb13afa4dcdf5551c61f187a8e7ceb52382642
Andrey Zotikov <andrey.zotikov@gmail.com>
2026-09-18 23:18:31 +0300

    QT internal zlib (backported from bitcoin/bitcoin#21376)

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,5 @@
 packages:=boost openssl libevent
 
-qt_packages = zlib
-
 qrencode_packages = qrencode
 
 qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
